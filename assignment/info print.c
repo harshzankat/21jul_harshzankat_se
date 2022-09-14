b15d54f74b5d414acdc1d5090daaea3848bc2939
harshzankat <109950061+harshzankat@users.noreply.github.com>
2022-09-14 15:10:49 +0530

    Delete info print.c

--- assignment/info print.c
+++
@@ -1,9 +0,0 @@
-# include<stdio.h>
-int main()
-{
-	printf("\n my name is Harsh Zankat ");
-	printf("\n my date of birth 03 may 2001");
-	printf("\n my age is 20 year");
-	printf("\n my address is gir somnath");	
-	return 0;
-}
